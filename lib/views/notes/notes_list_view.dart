62e0ac7158cb9aa4550f595a013324015a3ef686
Nguyễn Quốc Anh <quocanhseu@gmail.com>
2022-04-27 22:33:04 +0700

    step-17

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:learningdart/services/crud/notes_service.dart';
 import 'package:learningdart/utilities/dialogs/delete_dialog.dart';
 
-typedef DeleteNoteCallBack = void Function(DatabaseNote note);
+typedef NoteCallBack = void Function(DatabaseNote note);
 
 class NotesListView extends StatelessWidget {
   final List<DatabaseNote> notes;
-  final DeleteNoteCallBack onDeleteNote;
+  final NoteCallBack onDeleteNote;
+  final NoteCallBack onTap;
 
   const NotesListView({
     Key? key,
     required this.notes,
     required this.onDeleteNote,
+    required this.onTap,
   }) : super(key: key);
 
   @override
@@ -36,6 +38,9 @@ class NotesListView extends StatelessWidget {
             },
             icon: const Icon(Icons.delete),
           ),
+          onTap: () {
+            onTap(note);
+          },
         );
       },
     );
